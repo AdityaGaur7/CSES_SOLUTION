6cbad23d9f068cd6e8fb9b40c1027e394caeb9b3
AdityaGaur7 <adgaur027@gmail.com>
2023-11-09 22:02:48 +0530

    first commit

---
+++ INTODUCTORY/Number_spiral.cpp
@@ -0,0 +1,45 @@
+// author - Aditya gaur //
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define vv(v)        vector<int>v
+#define vvll(v)      vector<long long>v
+#define vp(x)      v.push_back(x)
+#define len(s)     (int)s.size()
+#define mp(x,y)    make_ pair(x,y)
+#define vpair      vector<pair<int,int>>vp
+#define srt(a)    sort(a.begin(),a.end())
+#define rev(a)     reverse(a.begin(),a.end())
+#define r(i,a,b)   for ( int i = a; i< b; i++)
+#define feach(a)   for(auto x: a)cout<<x<<" "
+#define print(x)   cout<<x<<endl
+#define tc         int t;cin>>t;while(t--)
+#define maxi(arr)  *max_element(arr.begin(),arr.end())
+#define mini(arr)  *min_element(arr.begin(),arr.end())
+#define ll         long long 
+#define mod        1000000007
+#define lwr        transform(s.begin(), s.end(), s.begin(), ::tolower);
+bool iss(const string& x, const string& s) {
+    size_t pos = x.find(s);
+    return pos != string::npos;
+}
+
+void solve(){
+    long long t;
+    cin>>t;
+    while(t--){
+
+       long long y,x;
+       cin>>y>>x;
+       cout<<a[y-1][x-1]<<endl;
+
+
+    }
+
+}
+
+
+int main(){
+   solve();
+    return 0;
+}
